8b30206f8e8386188cdcb0c8d1168338b1458c85
Alexey Mytsykov <alexmyt1@gmail.com>
2023-06-25 09:25:29 +0300

    chore: remove unused app controller ans service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RoomsModule } from './modules/rooms/rooms.module';
 import { ConfigModule } from './lib/config/config.module';
 import { OrmModule } from './lib/orm.module';
@@ -9,7 +7,5 @@ import { ReservationsModule } from './modules/reservations/reservations.module';
 
 @Module({
   imports: [ConfigModule, OrmModule, RoomsModule, ReservationsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
