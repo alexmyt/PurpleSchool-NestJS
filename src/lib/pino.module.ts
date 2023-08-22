889679b37e177dacd817a01cd221e166703ae6bf
Alexey Mytsykov <alexmyt1@gmail.com>
2023-08-22 16:14:26 +0300

    ci: move logs from root to folder

--- src/lib/pino.module.ts
+++ src/lib/pino.module.ts
@@ -12,27 +12,56 @@ const basePinoOptions = {
 @Module({
   imports: [
     LoggerModule.forRootAsync({
-      useFactory: () => ({
-        pinoHttp: {
-          timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
-          redact: {
-            paths: redactFields,
-            censor: '**GDPR COMPLIANT**',
-          },
-          transport: {
-            targets: [
-              {
-                target: 'pino-pretty',
-                level: 'info', // log only info and above to console
-                options: {
-                  ...basePinoOptions,
-                  colorize: true,
+      useFactory: () => {
+        const isProdEnv = process.env.NODE_ENV && process.env.NODE_ENV.startsWith('prod');
+        const isTestEnv = process.env.NODE_ENV && process.env.NODE_ENV.startsWith('test');
+
+        return {
+          pinoHttp: {
+            timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+            redact: {
+              paths: redactFields,
+              censor: '**CENSORED**',
+            },
+            transport: isTestEnv
+              ? undefined
+              : isProdEnv
+              ? {
+                  target: 'pino/file',
+                  level: 'error', // log only errors to file
+                  options: {
+                    ...basePinoOptions,
+                    destination: 'logs/error.log',
+                    mkdir: true,
+                    sync: false,
+                  },
+                }
+              : {
+                  targets: [
+                    {
+                      target: 'pino-pretty',
+                      level: 'info', // log only info and above to console
+                      options: {
+                        ...basePinoOptions,
+                        colorize: true,
+                      },
+                    },
+                    {
+                      target: 'pino/file',
+                      level: 'error', // log only errors to file
+                      options: {
+                        ...basePinoOptions,
+                        destination: 'logs/error.log',
+                        mkdir: true,
+                        sync: false,
+                      },
+                    },
+                  ],
                 },
-              },
-            ],
+            level: isTestEnv ? 'silent' : 'info',
           },
-        },
-      }),
+        };
+      },
     }),
   ],
   exports: [LoggerModule],
