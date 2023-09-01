c0c7f63081ac71dea8f1914eab0f6fafa54c02ca
Alexey Mytsykov <alexmyt1@gmail.com>
2023-09-01 13:17:17 +0300

    fix: add deleted const

--- src/modules/reservations/reservations.constants.ts
+++ src/modules/reservations/reservations.constants.ts
@@ -10,3 +10,4 @@ export const TEMPLATES = {
 
 export const RESERVATION_CREATED_SUBJECT = 'Reservation created';
 export const RESERVATION_CANCELED_SUBJECT = 'Reservation canceled';
+export const RESERVATION_NOT_FOUND = 'Reservation not found';
